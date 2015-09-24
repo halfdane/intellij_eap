4b2cd2f091d8283b063eb5c8ef78ac5b9a833dae
halfdane <halfdane@gmx.net>
2015-09-24 12:55:26 +0200

    downloader

---
+++ config.rb
@@ -0,0 +1,16 @@
+# url to main confluence EAP page on jetbrains website
+EAP_URL = "/display/IDEADEV/IDEA+15+EAP"
+
+# this is where downloaded archive will be stored
+DOWNLOAD_PATH = "~/Downloads/intellij"
+
+# path to which IntelliJ distributive will be unpacked
+INSTALLATION_PATH = "~/intellij"
+
+# path to config files which will be copied to bin of IntelliJ installation
+# (this property is optional)
+#IDEA_CONFIG = "~/idea15config"
+
+# Path to link the untarred result to
+# (this property is optional)
+LINK_TO = "~/idea"
---
+++ eap_download.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/ruby
+require File.expand_path(File.dirname(__FILE__) + '/config')
+
+
+class Downloader
+    def download_idea force = false
+        download(last_build_download_urls, force)
+    end
+
+    private
+
+    def download url, force = false
+        filename = DOWNLOAD_PATH + "/" + url.gsub(/.+\//, '')
+        if File.exists? filename then
+            puts "#{url} is already downloaded: #{filename}"
+            return unless force
+            puts "download is forced"
+            `rm #{filename}`
+        end
+
+        puts "downloading #{url}..."
+        Dir.chdir(DOWNLOAD_PATH){ `wget #{url}` }
+    end
+
+    def last_build_download_urls
+        return @urls unless @urls.nil?
+
+        require 'net/http'
+        require 'uri'
+
+        download_url = nil
+        eap_page = nil
+
+        puts "checking latest EAP build"
+        url = URI.parse("http://confluence.jetbrains.com" + EAP_URL)
+        eap_page = Net::HTTP.start(url.host, url.port) do |http|
+            http.get(EAP_URL)
+        end
+
+        download_pattern = /.*(https\:\/\/download\.jetbrains\.com\/idea\/ideaIU[^"]+\.tar\.gz)".*/m
+        download_url = download_pattern.match(eap_page.body)[1]
+        puts "   found #{download_url}"
+
+        return (@urls = download_url)
+    end
+end
+
+
+force = ARGV.include?("f")
+
+d = Downloader.new
+d.download_idea(force)
