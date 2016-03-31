4e436e5a8da7b4808d2060dc39010e512a1b447a
Halfdane <halfdane@gmx.net>
2016-03-31 10:44:50 +0200

    adjust channel

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 # url to main confluence EAP page on jetbrains website
-EAP_URL = "/display/IDEADEV/IDEA+16+EAP"
+EAP_URL = "/display/IDEADEV/IDEA+2016.1+EAP"
 
 # this is where downloaded archive will be stored
 DOWNLOAD_PATH = "~/Downloads/intellij"
@@ -10,3 +10,5 @@ INSTALLATION_PATH = "~/intellij"
 # Path to link the untarred result to
 # (this property is optional)
 LINK_TO = "~/idea"
+
+#145.596.7
